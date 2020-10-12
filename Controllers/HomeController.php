59fad11f020820c1775e0bf6bceca4b2fa2e9ab6
GarciaMatias <72658093+GarciaMatias@users.noreply.github.com>
2020-10-12 17:55:15 -0300

    pruba api

--- Controllers/HomeController.php
+++ Controllers/HomeController.php
@@ -4,10 +4,19 @@ namespace Controllers;
 
 class HomeController{
 
-    public function Index(){
+   /* public function Index(){
         echo 'hola';
+    }*/
+
+    public function Index()
+    {echo "ola";
+        $now=curl_exec("https://api.themoviedb.org/3/movie/now_playing?api_key=b285f5e6eecdd8eda1b3f5a82415153b&language=en-US&page=1");
+        var_dump($now);
     }
 
+
 }
+
+
 
 ?>
