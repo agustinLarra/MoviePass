8b5d9bb9e1c5c02df6c0c38fd04f0d76b0167931
RinaGrasso <rinchugrasso@gmail.com>
2020-10-15 11:25:51 -0300

    commit

---
+++ processSignUp.php
@@ -0,0 +1,30 @@
+<?php
+    require_once("Config\Autoload.php");
+
+    use Controllers\UserController as UserController ;
+    use Models\User as User;
+
+    if($_POST)
+    {
+        //$id = $_POST[$id];
+        $firstName = $_POST[$firstName];
+        $lastName = $_POST[$lastName];
+        $dni = $_POST[$dni];
+        $email = $_POST[$email];
+        $password = $_POST[$password];
+
+        $user = new User();
+        $user->setFirstName($firstName);
+        $user->setLastName($lastName);
+        $user->setDni($dni);
+        $user->setEmail($email);
+        $user->setPassword($password);
+
+       $userController = new UserController();
+       
+       $userController->signUp($user);
+        
+    }
+
+    header("location:index.php");
+?>
